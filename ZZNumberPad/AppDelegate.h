baa63f3d4dea7a69f25c6064fd889e3fe586f85a
zhangjingjian <zhangjingjian@dabby.cn>
2018-11-23 10:11:23 +0800

    Initial Commit

---
+++ ZZNumberPad/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZZNumberPad
+//
+//  Created by dabby on 2018/11/23.
+//  Copyright © 2018 Jam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZZNumberPad/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZZNumberPad
+//
+//  Created by dabby on 2018/11/23.
+//  Copyright © 2018 Jam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
